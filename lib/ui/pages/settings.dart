6c90c38b300bd1af993198720722907b9b69ea6d
Mohamed-Essam97 <mohamedessam.me@hotmail.com>
2021-04-18 04:34:40 +0200

    done

--- lib/ui/pages/settings.dart
+++ lib/ui/pages/settings.dart
@@ -30,102 +30,105 @@ class SettingsPage extends StatelessWidget {
         model: SettingsPageModel(context: context),
         builder: (context, model, _child) => Scaffold(
               body: SafeArea(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: CachedNetworkImage(
-                        imageUrl: user.photoURL,
-                        imageBuilder: (context, imageProvider) => Container(
-                          width: screenWidth / 3,
-                          height: screenWidth / 3,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(80),
-                            image: DecorationImage(
-                              image: imageProvider,
-                              fit: BoxFit.cover,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: CachedNetworkImage(
+                          imageUrl: user.photoURL,
+                          imageBuilder: (context, imageProvider) => Container(
+                            width: screenWidth / 3,
+                            height: screenWidth / 3,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(80),
+                              image: DecorationImage(
+                                image: imageProvider,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          placeholder: (context, url) =>
+                              CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              Icon(Icons.error),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        user.displayName,
+                        style: TextStyle(
+                            color: Colors.blue[800],
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        user.email,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: TextFormField(
+                          controller: myController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Enter URL ',
+                            fillColor: Colors.white,
+                            contentPadding: const EdgeInsets.only(
+                                left: 14.0, bottom: 6.0, top: 8.0),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
                           ),
                         ),
-                        placeholder: (context, url) =>
-                            CircularProgressIndicator(),
-                        errorWidget: (context, url, error) => Icon(Icons.error),
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      user.displayName,
-                      style: TextStyle(
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: NormalButton(
+                          text: "GO TO WEBSITE",
                           color: Colors.blue[800],
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      user.email,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: TextFormField(
-                        controller: myController,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Enter URL ',
-                          fillColor: Colors.white,
-                          contentPadding: const EdgeInsets.only(
-                              left: 14.0, bottom: 6.0, top: 8.0),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
+                          onPressed: () {
+                            myController.text.isEmpty
+                                ? UI.toast("please enter url")
+                                : UI.push(
+                                    context,
+                                    WebViewPage(
+                                      webUrl: myController.text,
+                                    ));
+                          },
+                          height: 30,
+                          // width: 80,
+                          raduis: 8,
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: NormalButton(
-                        text: "GO TO WEBSITE",
-                        color: Colors.blue[800],
-                        onPressed: () {
-                          myController.text.isEmpty
-                              ? UI.toast("please enter url")
-                              : UI.push(
-                                  context,
-                                  WebViewPage(
-                                    webUrl: myController.text,
-                                  ));
-                        },
-                        height: 30,
-                        // width: 80,
-                        raduis: 8,
-                      ),
-                    ),
-                    Text("You Have ${model.devices.length} Device"),
-                    DropdownButton<String>(
-                        items: model.devices.map((String val) {
-                          return DropdownMenuItem<String>(
-                            value: val,
-                            child: new Text(val),
-                          );
-                        }).toList(),
-                        hint: Text(dropdownValue),
-                        onChanged: (String val) {
-                          dropdownValue = val;
-                          model.setState();
-                        })
-                  ],
+                      Text("You Have ${model.devices.length} Device"),
+                      DropdownButton<String>(
+                          items: model.devices.map((String val) {
+                            return DropdownMenuItem<String>(
+                              value: val,
+                              child: new Text(val),
+                            );
+                          }).toList(),
+                          hint: Text(dropdownValue),
+                          onChanged: (String val) {
+                            dropdownValue = val;
+                            model.setState();
+                          })
+                    ],
+                  ),
                 ),
               ),
             ));
