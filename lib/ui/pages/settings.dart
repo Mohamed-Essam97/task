3c2ad31157a67933e05fa7e0efd5f4fc29a253bd
Mohamed-Essam97 <mohamedessam.me@hotmail.com>
2021-04-18 04:31:42 +0200

    done

--- lib/ui/pages/settings.dart
+++ lib/ui/pages/settings.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:base_notifier/base_notifier.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -7,6 +9,10 @@ import 'package:testapp/ui/pages/webView.dart';
 import 'package:testapp/ui/widgets/buttons/normal_button.dart';
 import 'package:ping_discover_network/ping_discover_network.dart';
 import 'package:ui_utils/ui_utils.dart';
+import 'package:http/http.dart' as http;
+import 'package:ping_discover_network/ping_discover_network.dart';
+import 'package:wifi_info_flutter/wifi_info_flutter.dart';
+import 'package:wifi/wifi.dart';
 
 class SettingsPage extends StatelessWidget {
   User user;
@@ -18,6 +24,9 @@ class SettingsPage extends StatelessWidget {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
     return BaseWidget<SettingsPageModel>(
+        initState: (m) => WidgetsBinding.instance.addPostFrameCallback((_) {
+              m.checkPortRange('192.168.0', 80, 200);
+            }),
         model: SettingsPageModel(context: context),
         builder: (context, model, _child) => Scaffold(
               body: SafeArea(
@@ -103,11 +112,26 @@ class SettingsPage extends StatelessWidget {
                         raduis: 8,
                       ),
                     ),
+                    Text("You Have ${model.devices.length} Device"),
+                    DropdownButton<String>(
+                        items: model.devices.map((String val) {
+                          return DropdownMenuItem<String>(
+                            value: val,
+                            child: new Text(val),
+                          );
+                        }).toList(),
+                        hint: Text(dropdownValue),
+                        onChanged: (String val) {
+                          dropdownValue = val;
+                          model.setState();
+                        })
                   ],
                 ),
               ),
             ));
   }
+
+  String dropdownValue = "Choose";
 }
 
 class SettingsPageModel extends BaseNotifier {
@@ -117,6 +141,55 @@ class SettingsPageModel extends BaseNotifier {
     NotifierState state,
     this.context,
   }) : super(state: state);
+
+  void checkPortRange(String subnet, int fromPort, int toPort) async {
+    final String ip = await Wifi.ip;
+    final String subnet = ip.substring(0, ip.lastIndexOf('.'));
+    final int port = 80;
+
+    final stream = NetworkAnalyzer.discover2(subnet, port);
+    stream.listen((NetworkAddress addr) {
+      if (addr.exists) {
+        print('Found device: ${addr.ip}');
+        devices.add(addr.ip);
+        setState();
+      }
+    });
+  }
+
+  List<String> devices = [];
+
+  // void _testConnection(
+  //   final http.Client client,
+  //   final String ip,
+  // ) async {
+  //   try {
+  //     final response = await client.get(Uri.parse('http://$ip/helloWorld'));
+
+  //     if (response.statusCode == 200) {
+  //       if (!response.body.startsWith("<")) {
+  //         if (!devices.contains(ip)) {
+  //           print('Found a device with ip $ip! Adding it to list of devices');
+  //           List<Device> containedDevices = [];
+  //           for (Device device in devices) {
+  //             if (device.ipAddress.compareTo(ip) == 0) {
+  //               containedDevices.add(device);
+  //             }
+  //           }
+
+  //           if (containedDevices.length == 0) {
+  //             devices.add(
+  //               Device(ip),
+  //             );
+  //             setState();
+  //           }
+  //         }
+  //       }
+  //     }
+  //   } on SocketException catch (e) {
+  //     //NOP
+  //   }
+  // }
 
 //   fun() async{
 //   final String ip = await Wifi.ip;
